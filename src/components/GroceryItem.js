ee8d7c7d94473f2b97981d6b221505146d082d83
Keeley Hammond <keeleymhammond@gmail.com>
2017-05-29 20:27:08 -0700

    Changes to grocery lost components

---
+++ src/components/GroceryItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class GroceryItem extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { ingredients } = this.props;
+    return (
+      <div></div>
+    );
+  }
+}
